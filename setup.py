5da2675fb2ff422aa625103a9927d783ba6783bf
Preston Moore <prestonkmoore@gmail.com>
2017-11-10 20:59:37 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup, Extension
+
+
+setup(name='tracereplay',
+      version='0.1',
+      description='Replay a system call trace through an application',
+      packages=['tracereplay', 'tracereplay.checker'],
+      ext_modules=[Extension('tracereplay.cinterface',
+                             ['tracereplay.c'])])
