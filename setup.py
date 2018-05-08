ca99d5b095849379fdb8f4bece85c5d9973d0c81
Preston Moore <prestonkmoore@gmail.com>
2018-05-08 13:58:42 -0700

    All warnings, c11

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup, Extension
 
 
+syscallreplay_mod = Extension('syscallreplay',
+                              ['syscallreplay.c'],
+                              extra_compile_args=['-Wall', '--std=c11'])
+
 setup(name='syscallreplay',
       version='0.1',
       description='Replay a system call trace through an application',
       packages=['syscallreplay'],
-      ext_modules=[Extension('syscallreplay',
-                             ['syscallreplay.c'])])
+      ext_modules=[syscallreplay_mod])
