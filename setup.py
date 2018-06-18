2ec1956ce65445e2290d10d63a3b03e910431b3e
Preston Moore <prestonkmoore@gmail.com>
2018-06-18 13:20:11 -0700

    Reorganize into better package layout

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from distutils.core import setup, Extension
 
 
-SYSCALLREPLAY_MOD = Extension('syscallreplay',
-                              ['syscallreplay.c'],
+SYSCALLREPLAY_MOD = Extension('syscallreplay.syscallreplay',
+                              ['syscallreplay/syscallreplay.c'],
                               extra_compile_args=['-Wall', '--std=c11'])
 
 setup(name='syscallreplay',
       version='0.1',
+      packages=['syscallreplay'],
       description='Replay a system call trace through an application',
       ext_modules=[SYSCALLREPLAY_MOD])
