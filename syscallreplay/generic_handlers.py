d2df743aa07ec41265088f393b4840d1ad53a13f
Preston Moore <prestonkmoore@gmail.com>
2018-07-10 11:28:55 -0700

    Make imports comply with programming standards

--- syscallreplay/generic_handlers.py
+++ syscallreplay/generic_handlers.py
@@ -3,15 +3,11 @@
 behavior
 """
 
+import logging
 
-from .errno_dict import ERRNO_CODES
-
-from .util import (ReplayDeltaError,
-                   logging,
-                   cint,
-                   noop_current_syscall,
-                   cleanup_return_value,
-                   apply_return_conditions,)
+import errno_dict
+import util as util
+import syscallreplay
 
 
 def syscall_return_success_handler(syscall_id, syscall_object, pid):
@@ -29,8 +25,8 @@ def syscall_return_success_handler(syscall_id, syscall_object, pid):
     """
 
     logging.debug('Using default "return success" handler')
-    noop_current_syscall(pid)
-    apply_return_conditions(pid, syscall_object)
+    util.noop_current_syscall(pid)
+    util.apply_return_conditions(pid, syscall_object)
 
 
 def check_return_value_entry_handler(syscall_id, syscall_object, pid):
@@ -46,8 +42,9 @@ def check_return_value_entry_handler(syscall_id, syscall_object, pid):
 
     """
     logging.debug('check_return_value entry handler')
-    logging.debug('Letting system call {}: {} pass through'
-                  .format(syscall_id, syscall_object.name))
+    logging.debug('Letting system call %d : %s pass through',
+                  syscall_id,
+                  syscall_object.name)
 
 
 def check_return_value_exit_handler(syscall_id, syscall_object, pid):
@@ -64,25 +61,25 @@ def check_return_value_exit_handler(syscall_id, syscall_object, pid):
 
     """
     logging.debug('check_return_value exit handler')
-    ret_from_execution = cint.peek_register(pid, cint.EAX)
-    ret_from_trace = cleanup_return_value(syscall_object.ret[0])
+    ret_from_execution = syscallreplay.peek_register(pid, syscallreplay.EAX)
+    ret_from_trace = util.cleanup_return_value(syscall_object.ret[0])
     logging.debug('Return value from execution %x', ret_from_execution)
     logging.debug('Return value from trace %x', ret_from_trace)
     # HACK HACK HACK
     if syscall_object.ret[1] is not None:
         logging.debug('We have an errno code')
         logging.debug('Errno code: %s', syscall_object.ret[1])
-        errno_retval = -1 * ERRNO_CODES[syscall_object.ret[1]]
+        errno_retval = -1 * errno_dict.ERRNO_CODES[syscall_object.ret[1]]
         logging.debug('Errno ret_val: %d', errno_retval)
         if errno_retval == ret_from_execution:
             return
     if ret_from_execution < 0:
         ret_from_execution &= 0xffffffff
     if ret_from_execution != ret_from_trace:
-        raise ReplayDeltaError('Return value from execution ({}, {:02x}) '
-                               'differs from return value from trace '
-                               '({}, {:02x})'
-                               .format(ret_from_execution,
-                                       ret_from_execution,
-                                       ret_from_trace,
-                                       ret_from_trace))
+        raise util.ReplayDeltaError('Return value from execution ({}, {:02x}) '
+                                    'differs from return value from trace '
+                                    '({}, {:02x})'
+                                    .format(ret_from_execution,
+                                            ret_from_execution,
+                                            ret_from_trace,
+                                            ret_from_trace))
