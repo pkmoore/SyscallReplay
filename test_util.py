db88a2c2750d3d72f8fceccb1e6f6ecd8d2a8541
Preston Moore <prestonkmoore@gmail.com>
2018-07-15 19:16:25 -0700

    noop_current_syscall: test happy case and noop_fails

--- test_util.py
+++ test_util.py
@@ -126,3 +126,67 @@ class TestStopForDebug(unittest.TestCase):
     mock_log.assert_called()
     mock_kill.assert_called_with(pid, mock_signal)
     mock_syscallreplay.detach.assert_called_with(pid)
+
+
+
+
+
+class TestNoopCurrentSyscall(unittest.TestCase):
+
+  @mock.patch('logging.debug')
+  @mock.patch('syscallreplay.util.next_syscall')
+  @mock.patch('syscallreplay.util.cint')
+  def test_with_successful_noop(self, mock_syscallreplay, mock_next, mock_log):
+    """Test for correct noop with valid pid
+    <Purpose>
+      Ensure a correct noop process happens when passed a valid pid
+
+    """
+    pid = 555
+    getpid_syscallno = 20
+    ptrace_syscall_signalno = 0
+
+    mock_syscallreplay.poke_register = mock.Mock()
+    mock_syscallreplay.ORIG_EAX = mock.Mock()
+    mock_syscallreplay.syscall = mock.Mock()
+    mock_syscallreplay.peek_register = mock.Mock(return_value=getpid_syscallno)
+    mock_syscallreplay.entering_syscall = True
+
+    syscallreplay.util.noop_current_syscall(pid)
+
+    mock_log.assert_called()
+    mock_syscallreplay.poke_register.assert_called_with(pid, mock_syscallreplay.ORIG_EAX, getpid_syscallno)
+    mock_syscallreplay.syscall.assert_called_with(pid, ptrace_syscall_signalno)
+    mock_next.assert_called()
+    mock_syscallreplay.peek_register(pid, mock_syscallreplay.ORIG_EAX)
+    self.assertEqual(mock_syscallreplay.entering_syscall, False)
+
+
+  @mock.patch('logging.debug')
+  @mock.patch('syscallreplay.util.next_syscall')
+  @mock.patch('syscallreplay.util.cint')
+  def test_for_exception_when_noop_fails(self, mock_syscallreplay, mock_next, mock_log):
+    """Make sure an exception is raised when nooping doesn't go as expected
+    <Purpose>
+      Make sure an exception is raised when nooping doesn't go as expected
+
+    """
+    pid = 555
+    getpid_syscallno = 20
+    bad_syscallno = -1
+    ptrace_syscall_signalno = 0
+
+    mock_syscallreplay.poke_register = mock.Mock()
+    mock_syscallreplay.ORIG_EAX = mock.Mock()
+    mock_syscallreplay.syscall = mock.Mock()
+    mock_syscallreplay.peek_register = mock.Mock(return_value=bad_syscallno)
+    mock_syscallreplay.entering_syscall = True
+
+    self.assertRaises(Exception, syscallreplay.util.noop_current_syscall, pid)
+
+    mock_log.assert_called()
+    mock_syscallreplay.poke_register.assert_called_with(pid, mock_syscallreplay.ORIG_EAX, getpid_syscallno)
+    mock_syscallreplay.syscall.assert_called_with(pid, ptrace_syscall_signalno)
+    mock_next.assert_called()
+    mock_syscallreplay.peek_register(pid, mock_syscallreplay.ORIG_EAX)
+    self.assertEqual(mock_syscallreplay.entering_syscall, True)
