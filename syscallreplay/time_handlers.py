b47bd1a5d4fb6de7658f5baaa2a292230369ee67
Preston Moore <prestonkmoore@gmail.com>
2018-12-04 16:32:25 -0800

    fix clock_gettime with labels

--- syscallreplay/time_handlers.py
+++ syscallreplay/time_handlers.py
@@ -505,13 +505,13 @@ def clock_gettime_entry_handler(syscall_id, syscall_object, pid):
         raise util.ReplayDeltaError('Clock type ({}) from execution '
                                  'differs from trace'
                                  .format(clock_type_from_execution))
-    seconds = syscall_object.args[1].value.strip('{}')
     # clock_gettime() call might have the tv_sec and tv_nsec labels in the
     # output structure.  If it does, we need to split() it off.
+    seconds = syscall_object.args[1].value.strip('{}')
     if 'tv_sec' in seconds:
       seconds = seconds.split('=')[1]
     seconds = int(seconds)
-    nanoseconds = syscall_object.args[1].value.strip('{}')
+    nanoseconds = syscall_object.args[2].value.strip('{}')
     if 'tv_nsec' in nanoseconds:
       nanoseconds = nanoseconds.split('=')[1]
     nanoseconds = int(nanoseconds)
